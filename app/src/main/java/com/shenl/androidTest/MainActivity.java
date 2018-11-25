70c6faa3017a0ffe0b3b8b79fc497e2df886225a
Administrator <286388725@qq.com>
2018-11-25 20:59:55 +0800

    提交修改

--- app/src/main/java/com/shenl/androidTest/MainActivity.java
+++ app/src/main/java/com/shenl/androidTest/MainActivity.java
@@ -1,16 +1,10 @@
 package com.shenl.androidTest;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-import com.shenl.utils.MyUtils.PhoneUtils;
 import com.shenl.utils.activity.BaseActivity;
-
 public class MainActivity extends BaseActivity {
 
-    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +17,6 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void initView() {
-        btn = findViewById(R.id.btn);
     }
 
     @Override
@@ -33,12 +26,7 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void initEvent() {
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                PhoneUtils.getChoosedPhoneNumber(MainActivity.this,1,new Intent());
-            }
-        });
+
     }
 
 }
