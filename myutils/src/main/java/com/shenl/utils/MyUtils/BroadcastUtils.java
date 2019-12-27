36b2883f9383d51fe7319386882fb9041f143a67
shenl <shenl@neuqsoft.com>
2019-12-27 15:04:11 +0800

    修改代码

--- myutils/src/main/java/com/shenl/utils/MyUtils/BroadcastUtils.java
+++ myutils/src/main/java/com/shenl/utils/MyUtils/BroadcastUtils.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Bundle;
 
 public class BroadcastUtils {
 
@@ -30,8 +31,22 @@ public class BroadcastUtils {
      * 创建时间:   2019/11/22
      */
     public static void SendBroadcas(Context context,String action){
+        SendBroadcas(context,action,null);
+    }
+
+    /**
+     * TODO 功能：发送广播
+     *
+     * 参数说明:
+     * 作    者:   沈  亮
+     * 创建时间:   2019/12/27
+     */
+    public static void SendBroadcas(Context context,String action,Bundle bundle){
         Intent intent = new Intent();
         intent.setAction(action);
+        if (bundle != null){
+            intent.putExtras(bundle);
+        }
         context.sendBroadcast(intent);
     }
 
