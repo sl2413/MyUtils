6ac87d882c2123fbb9aeb652abbf721ce1eccecc
shenl <shenl@neuqsoft.com>
2020-05-29 10:39:03 +0800

    ...

--- myutils/src/main/java/com/shenl/utils/MyCallback/LongLinkCallBack.java
+++ myutils/src/main/java/com/shenl/utils/MyCallback/LongLinkCallBack.java
@@ -3,10 +3,12 @@ package com.shenl.utils.MyCallback;
 import org.java_websocket.handshake.ServerHandshake;
 
 public abstract class LongLinkCallBack {
-
+    //长连接开启
     public void onOpen(ServerHandshake handshakedata){}
     // 收到消息回调
     public abstract void success(String message);
+    //长连接关闭
     public void onClose(int code, String reason, boolean remote){}
+    //长连接异常
     public void onError(Exception ex){}
 }
