97fce3cec3d80893d6b4646b031364b9e917e00b
a286388725 <286388725@qq.com>
2020-06-30 22:46:17 +0800

    ...

--- myutils/src/main/java/com/shenl/utils/application/AppManager.java
+++ myutils/src/main/java/com/shenl/utils/application/AppManager.java
@@ -3,96 +3,124 @@ package com.shenl.utils.application;
 import android.app.Activity;
 import android.content.Context;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 public class AppManager {// Activity栈  
-    private static Stack<Activity> activityStack;  
+    private static Stack<Activity> activityStack;
     // 单例模式  
-    private static AppManager instance;  
+    private static AppManager instance;
 
-    private AppManager() {  
-    }  
+    private AppManager() {
+    }
 
-    /** 
-     * 单一实例 
-     */  
-    public static AppManager getAppManager() {  
-        if (instance == null) {  
-            instance = new AppManager();  
-        }  
-        return instance;  
-    }  
+    /**
+     * 单一实例
+     */
+    public static AppManager getAppManager() {
+        if (instance == null) {
+            instance = new AppManager();
+        }
+        return instance;
+    }
 
-    /** 
-     * 添加Activity到堆栈 
-     */  
-    public void addActivity(Activity activity) {  
-        if (activityStack == null) {  
-            activityStack = new Stack<Activity>();  
-        }  
-        activityStack.add(activity);  
-    }  
+    /**
+     * 添加Activity到堆栈
+     */
+    public void addActivity(Activity activity) {
+        if (activityStack == null) {
+            activityStack = new Stack<Activity>();
+        }
+        activityStack.add(activity);
+    }
 
-    /** 
-     * 获取当前Activity（堆栈中最后一个压入的） 
-     */  
-    public Activity currentActivity() {  
-        Activity activity = activityStack.lastElement();  
-        return activity;  
-    }  
+    /**
+     * 获取当前Activity（堆栈中最后一个压入的）
+     */
+    public Activity currentActivity() {
+        Activity activity = activityStack.lastElement();
+        return activity;
+    }
 
-    /** 
-     * 结束当前Activity（堆栈中最后一个压入的） 
-     */  
-    public void finishActivity() {  
-        Activity activity = activityStack.lastElement();  
-        finishActivity(activity);  
-    }  
+    /**
+     * 结束当前Activity（堆栈中最后一个压入的）
+     */
+    public void finishActivity() {
+        Activity activity = activityStack.lastElement();
+        finishActivity(activity);
+    }
 
-    /** 
-     * 结束指定的Activity 
-     */  
-    public void finishActivity(Activity activity) {  
-        if (activity != null) {  
-            activityStack.remove(activity);  
-            activity.finish();  
-            activity = null;  
-        }  
-    }  
+    /**
+     * 结束指定的Activity
+     */
+    public void finishActivity(Activity activity) {
+        if (activity != null) {
+            activityStack.remove(activity);
+            activity.finish();
+            activity = null;
+        }
+    }
 
-    /** 
-     * 结束指定类名的Activity 
-     */  
-    public void finishActivity(Class<?> cls) {  
-        for (Activity activity : activityStack) {  
-            if (activity.getClass().equals(cls)) {  
+    /**
+     * 结束指定类名的Activity
+     */
+    public void finishActivity(Class<?> cls) {
+        for (Activity activity : activityStack) {
+            if (activity.getClass().equals(cls)) {
                 finishActivity(activity);
                 break;
-            }  
-        }  
-    }  
+            }
+        }
+    }
 
-    /** 
-     * 结束所有Activity 
-     */  
-    public void finishAllActivity() {  
-        for (int i = 0; i < activityStack.size(); i++) {  
-            if (null != activityStack.get(i)) {  
-                activityStack.get(i).finish();  
-            }  
-        }  
-        activityStack.clear();  
-    }  
+    /**
+     * 结束所有Activity
+     */
+    public void finishAllActivity() {
+        for (int i = 0; i < activityStack.size(); i++) {
+            if (null != activityStack.get(i)) {
+                activityStack.get(i).finish();
+            }
+        }
+        activityStack.clear();
+    }
 
-    /** 
-     * 退出应用程序 
-     */  
-    public void AppExit(Context context) {  
-        try {  
-            finishAllActivity();  
+    /**
+     * 退出应用程序
+     */
+    public void AppExit(Context context) {
+        try {
+            finishAllActivity();
             //退出程序
             android.os.Process.killProcess(android.os.Process.myPid());
-            System.exit(1); 
-        } catch (Exception e) {  
-        }  
-    }  }
+            System.exit(1);
+        } catch (Exception e) {
+        }
+    }
+
+
+    /**
+     * finish指定的activity之上所有的activity
+     */
+    public boolean finishToActivity(Class<? extends Activity> actCls, boolean isIncludeSelf) {
+        List<Activity> buf = new ArrayList<Activity>();
+        int size = activityStack.size();
+        Activity activity = null;
+        for (int i = size - 1; i >= 0; i--) {
+            activity = activityStack.get(i);
+            if (activity.getClass().isAssignableFrom(actCls)) {
+                for (Activity a : buf) {
+                    a.finish();
+                }
+                return true;
+            } else if (i == size - 1 && isIncludeSelf) {
+                buf.add(activity);
+            } else if (i != size - 1) {
+                buf.add(activity);
+            }
+        }
+        return false;
+    }
+
+}
